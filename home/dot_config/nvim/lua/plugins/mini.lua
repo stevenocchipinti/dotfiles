e03cf2cc98c90aee7caf1f3172ca8a787518ea1a
Steve Occhipinti <dev@stevenocchipinti.com>
2025-11-17 12:25:26 +1100

    Updates from Omarchy
    The `\d` works by default on mac but not linux, so switching back to `[0-9]`

--- home/dot_config/nvim/lua/plugins/mini.lua
+++ home/dot_config/nvim/lua/plugins/mini.lua
@@ -1,39 +1,39 @@
 return {
-  {
-    "nvim-mini/mini.pairs",
-    opts = function(_, opts)
-      -- This code only completes the pair if the cursor is at the end of the
-      -- line, chatGPT wrote it but I want to see if this stops the annoying
-      -- behaviour of autocompleting a closing bracket when I'm just adding a
-      -- starting bracket for a closing bracket that already exists.
-
-      -- Disable default character-based skip logic
-      opts.skip_next = nil
-
-      -- We'll save a reference to the original open function later
-      local original_open = nil
-
-      -- Schedule override after mini.pairs has been set up
-      vim.schedule(function()
-        local pairs = require("mini.pairs")
-        original_open = original_open or pairs.open
-
-        ---@diagnostic disable-next-line: duplicate-set-field
-        pairs.open = function(pair, neigh_pattern)
-          local line = vim.api.nvim_get_current_line()
-          local col = vim.fn.col(".")
-
-          -- If the cursor is not at the end of the line, skip pairing
-          if col <= #line then
-            return pair:sub(1, 1)
-          end
-
-          -- Call the original open function
-          return original_open(pair, neigh_pattern)
-        end
-      end)
-    end,
-  },
+  -- {
+  --   "nvim-mini/mini.pairs",
+  --   opts = function(_, opts)
+  --     -- This code only completes the pair if the cursor is at the end of the
+  --     -- line, chatGPT wrote it but I want to see if this stops the annoying
+  --     -- behaviour of autocompleting a closing bracket when I'm just adding a
+  --     -- starting bracket for a closing bracket that already exists.
+  --
+  --     -- Disable default character-based skip logic
+  --     opts.skip_next = nil
+  --
+  --     -- We'll save a reference to the original open function later
+  --     local original_open = nil
+  --
+  --     -- Schedule override after mini.pairs has been set up
+  --     vim.schedule(function()
+  --       local pairs = require("mini.pairs")
+  --       original_open = original_open or pairs.open
+  --
+  --       ---@diagnostic disable-next-line: duplicate-set-field
+  --       pairs.open = function(pair, neigh_pattern)
+  --         local line = vim.api.nvim_get_current_line()
+  --         local col = vim.fn.col(".")
+  --
+  --         -- If the cursor is not at the end of the line, skip pairing
+  --         if col <= #line then
+  --           return pair:sub(1, 1)
+  --         end
+  --
+  --         -- Call the original open function
+  --         return original_open(pair, neigh_pattern)
+  --       end
+  --     end)
+  --   end,
+  -- },
 
   {
     "nvim-mini/mini.files",
